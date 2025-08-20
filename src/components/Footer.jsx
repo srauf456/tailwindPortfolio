5890ee631365eb893b8f9ae1e28b82abafb27b77
srauf456 <srauf456@gmail.com>
2025-08-19 23:53:30 -0500

    second commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-900 text-white py-4 text-center mt-auto">
+        <p>&copy; {new Date().getFullYear()} Your Name. All Rights Reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
