b5bcf178eda54f6c44b7f089ae9ec331cf789b92
srauf456 <srauf456@gmail.com>
2025-08-21 22:47:00 -0500

    fixed vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode, React } from 'react'
 import { createRoot, ReactDOM } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './styles.css'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
